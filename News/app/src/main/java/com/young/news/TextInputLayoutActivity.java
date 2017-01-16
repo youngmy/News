f0f0e7b8c0c2dd008abb972ffb355b8dea460e44
youngmy <504591775@qq.com>
2017-01-16 14:29:56 +0800

    第二次提交
    第二次提交

---
+++ News/app/src/main/java/com/young/news/TextInputLayoutActivity.java
@@ -0,0 +1,24 @@
+package com.young.news;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by mr.young on 2017/1/16.
+ */
+
+public class TextInputLayoutActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_input_layout);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
